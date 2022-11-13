49fc537df57662b1e108ae065b1b0193c83ecb8a
BobrikAU <kirbob0@yandex.ru>
2022-11-13 22:53:08 +0300

    feature: eine Seite machen

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
 import './pages/index.css';
+
+const buttonMenu = document.querySelector('.header__button');
+const modalNav = document.querySelector('.modal-nav');
+const buttonModal = document.querySelector('.modal-nav__button');
+const schowModalNav = () => {
+  modalNav.classList.add('modal-nav_function_open');
+};
+const closeModalNav = () => {
+  modalNav.classList.add('modal-nav_function_close');
+  setTimeout(() => {
+    modalNav.classList.remove('modal-nav_function_open');
+    modalNav.classList.remove('modal-nav_function_close');
+  }, 400);
+};
+const closeModalNavUsingLink = (e) => {
+  if (e.target.classList.contains('nav-list__link')) {
+    closeModalNav();
+  }
+};
+buttonMenu.addEventListener('click', schowModalNav);
+buttonModal.addEventListener('click', closeModalNav);
+modalNav.addEventListener('click', closeModalNavUsingLink);
