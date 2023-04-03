15813a714b8451a4933088e7e07921d5b2f8e930
BobrikAU <kirbob0@yandex.ru>
2023-04-03 23:28:48 +0300

    feature: оставшиеся документы, кнопка наверх

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import './pages/index.css';
 const buttonMenu = document.querySelector('.header__button');
 const modalNav = document.querySelector('.modal-nav');
 const buttonModal = document.querySelector('.modal-nav__button');
+const buttonLinkStart = document.querySelector('.main__link_form_button');
+const textContainer = document.querySelector('.main__text-container')
 const schowModalNav = () => {
   modalNav.classList.add('modal-nav_function_open');
 };
@@ -18,6 +20,14 @@ const closeModalNavUsingLink = (e) => {
     closeModalNav();
   }
 };
+const showCloseButtonLinkStart = () => {
+  if(textContainer.scrollTop > 700) {
+    buttonLinkStart.classList.add('main__link_show');
+  } else {
+    buttonLinkStart.classList.remove('main__link_show');
+  }
+};
 buttonMenu.addEventListener('click', schowModalNav);
 buttonModal.addEventListener('click', closeModalNav);
 modalNav.addEventListener('click', closeModalNavUsingLink);
+textContainer.addEventListener('scroll', showCloseButtonLinkStart)
