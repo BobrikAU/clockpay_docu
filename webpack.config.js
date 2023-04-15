bef8d23a2db0cbd9f5dc904207e1f6fc18289e77
BobrikAU <kirbob0@yandex.ru>
2023-04-15 23:45:37 +0300

    refactoring: change file structure, scripts und rules for generating markup files

--- webpack.config.js
+++ webpack.config.js
@@ -3,74 +3,83 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = {
-  entry: {
-    main: './src/index.js'
-  },
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'main.js',
-    publicPath: ''
-  },
-  mode: 'development',
-  devServer: {
-    static: path.resolve(__dirname, './dist'),
-    compress: true,
-    port: 8080,
-    open: true
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        use: 'babel-loader',
-        exclude: '/node_modules'
-      },
-      {
-        test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
-        type: 'asset/resource'
-      },
-      {
-        test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, {
-          loader: 'css-loader',
-          options: { importLoaders: 1 }
+module.exports = (env, argv) => {
+  console.log(`Mode ${argv.mode}`)
+  return {
+    entry: {
+      index: './src/index.js'
+    },
+    output: {
+      path: path.resolve(__dirname, 'dist/docs'),
+      filename: 'bundle.js',
+      publicPath: ''
+    },
+    devServer: {
+      static: path.resolve(__dirname, './dist/docs'),
+      compress: true,
+      port: 8080,
+      open: ['/rules']
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          use: 'babel-loader',
+          exclude: '/node_modules'
         },
-        'postcss-loader'
-        ]
-      }
+        {
+          test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
+          type: 'asset/resource'
+        },
+        {
+          test: /\.css$/,
+          use: [MiniCssExtractPlugin.loader, {
+            loader: 'css-loader',
+            options: { importLoaders: 1 }
+          },
+          'postcss-loader'
+          ]
+        }
+      ]
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        filename: `rules/index.${argv.mode === 'development' ? 'html' : 'php'}`,
+        template: './src/rules/index.html',
+        publicPath: '../'
+      }),
+      new HtmlWebpackPlugin({
+        filename: `license/index.${argv.mode === 'development' ? 'html' : 'php'}`,
+        template: './src/license/index.html',
+        publicPath: '../'
+      }),
+      new HtmlWebpackPlugin({
+        filename: `content/index.${argv.mode === 'development' ? 'html' : 'php'}`,
+        template: './src/content/index.html',
+        publicPath: '../'
+      }),
+      new HtmlWebpackPlugin({
+        filename: `policy/index.${argv.mode === 'development' ? 'html' : 'php'}`,
+        template: './src/policy/index.html',
+        publicPath: '../'
+      }),
+      new HtmlWebpackPlugin({
+        filename: `agreement/index.${argv.mode === 'development' ? 'html' : 'php'}`,
+        template: './src/agreement/index.html',
+        publicPath: '../'
+      }),
+      new HtmlWebpackPlugin({
+        filename: `confidentiality/index.${argv.mode === 'development' ? 'html' : 'php'}`,
+        template: './src/confidentiality/index.html',
+        publicPath: '../'
+      }),
+      new HtmlWebpackPlugin({
+        filename: `agency/index.${argv.mode === 'development' ? 'html' : 'php'}`,
+        template: './src/agency/index.html',
+        publicPath: '../'
+      }),
+      new CleanWebpackPlugin(),
+      new MiniCssExtractPlugin()
     ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      filename: 'index.html',
-      template: './src/index.html'
-    }),
-    new HtmlWebpackPlugin({
-      filename: 'license.html',
-      template: './src/license.html'
-    }),
-    new HtmlWebpackPlugin({
-      filename: 'publication.html',
-      template: './src/publication.html'
-    }),
-    new HtmlWebpackPlugin({
-      filename: 'privacy.html',
-      template: './src/privacy.html'
-    }),
-    new HtmlWebpackPlugin({
-      filename: 'approval.html',
-      template: './src/approval.html'
-    }),
-    new HtmlWebpackPlugin({
-      filename: 'payment.html',
-      template: './src/payment.html'
-    }),
-    new HtmlWebpackPlugin({
-      filename: 'agent.html',
-      template: './src/agent.html'
-    }),
-    new CleanWebpackPlugin(),
-    new MiniCssExtractPlugin()
-  ]
+  }
 }
